7158d9bba4134ce7146d66540b28c95423b2d557
odejinmisa <odejinmisa@newwavesecosystem.com>
2022-04-23 17:14:56 +0100

    push changes

--- app/Http/Controllers/VFDController.php
+++ app/Http/Controllers/VFDController.php
@@ -77,8 +77,11 @@ class VFDController extends Controller
 
     public function validateBankAccount(Request  $request){
 //return $request;
-        $bankCode=$request->bankcode;
-        $accountNumber=$request->number;
+//        $bankCode=$request->bankcode;
+//        $accountNumber=$request->number;
+
+        $bankCode="999058";
+        $accountNumber="0001744830";
 
         $auth=$this->auth_init();
         $token=$auth['access_token'];
@@ -107,6 +110,119 @@ class VFDController extends Controller
 //        echo $response;
         return $response;
     }
+
+    public function accountEnquiry(Request  $request){
+//return $request;
+//        $bankCode=$request->bankcode;
+//        $accountNumber=$request->number;
+
+        $bankCode="999058";
+        $accountNumber="0001744830";
+
+        $auth=$this->auth_init();
+        $token=$auth['access_token'];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => env('VFD_URL').'vfd-wallet/1.1/wallet2/transfer/recipient?transfer_type=inter&accountNo='.$accountNumber.'&bank='.$bankCode.'&wallet-credentials='.env('VFD_AUTH'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_SSL_VERIFYPEER=>false,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$token,
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+//        echo $response;
+        return $response;
+    }
+
+    public function accountTransfer(Request  $request){
+//return $request;
+//        $bankCode=$request->bankcode;
+//        $accountNumber=$request->number;
+//        $accountBVN=$request->accountbvn;
+//        $accountName=$request->accountname;
+//        $amount=$request->amount;
+//        $narration=$request->narration;
+//        $sessionID=$request->sessionID;
+
+        //for test
+        $bankCode="999058";
+        $accountNumber="0001744830";
+        $accountBVN="22222222223";
+        $accountName="OGBA, CHRISTOPHER CHINONYE";
+        $amount="100";
+        $narration="test trf";
+        $sessionID="999116190411110815131298994293";
+
+
+        $auth=$this->auth_init();
+        $token=$auth['access_token'];
+
+        $fromAccount=env("VFD_TRANSFER_ACCOUNTNO");
+        $reference=env('VFD_TRANSFER_CLIENT')."-".uniqid();
+
+        $signature=hash('sha512', $fromAccount.$accountNumber);
+
+        $payload='{
+  "fromSavingsId": "'.env("VFD_TRANSFER_ACCOUNTID").'",
+  "amount": "'.$amount.'",
+  "toAccount": "'.$accountNumber.'",
+  "fromBvn": "1000000000",
+  "signature": "'.$signature.'",
+  "fromAccount": "'.$fromAccount.'",
+  "toBvn": "'.$accountBVN.'",
+  "remark": "'.$narration.'",
+  "fromClientId": "'.env("VFD_TRANSFER_CLIENTID").'",
+  "fromClient": "'.env("VFD_TRANSFER_CLIENT").'",
+  "toKyc": "99",
+  "reference": "'.$reference.'",
+  "toClientId": "",
+  "toClient": "'.$accountName.'",
+  "toSession": "'.$sessionID.'",
+  "transferType": "inter",
+  "toBank": "'.$bankCode.'",
+  "toSavingsId": ""
+}';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => env('VFD_URL').'vfd-wallet/1.1/wallet2/transfer?source=pool&wallet-credentials='.env('VFD_AUTH'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $payload,
+            CURLOPT_SSL_VERIFYPEER=>false,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$token,
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+//        echo $response;
+        return $response;
+    }
+
+
 
     public function auth_init(){
 
